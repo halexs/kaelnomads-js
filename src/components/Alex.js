c959a95f8ce2d8bc570900be850c9aee79d05634
ahsu94 <Alex>
2022-01-25 19:00:21 -0700

    Phase one done. Next stop, amazon!

--- src/components/Alex.js
+++ src/components/Alex.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@mui/material/Typography';
 
 function Alex(props) {
 
   return (
-    <div>
-      Alex
+    <div style={{width: "60%", color: "white"}}>
+
+      <Typography id="transition-modal-title" variant="h6" component="h2">
+        About Me
+      </Typography>
+      <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+        Hi Everyone, I'm mostly a software developer with experience in System Adminstration and DevOps. I created this website while my partner is in school to have something to do. Here is my resume: *resume from s3 here*
+      </Typography>
+      <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+        Email: a.hsu94@gmail.com
+      </Typography>
     </div>
   );
 }
--- src/components/Keily.js
+++ src/components/Keily.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@mui/material/Typography';
 
 function Keily(props) {
 
   return (
-    <div>
-      Keily
+    <div style={{width: "60%", color: "white"}}>
+
+      <Typography id="transition-modal-title" variant="h6" component="h2">
+        About Me
+      </Typography>
+      <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+        This page is under construction
+      </Typography>
     </div>
   );
 }
