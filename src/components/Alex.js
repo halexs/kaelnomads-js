bcd7b4378589a43d6dd605d21d8419f6ec5684d2
ahsu94 <Alex>
2022-01-30 11:52:42 -0700

    Update resume link to new s3 bucket.

--- src/components/Alex.js
+++ src/components/Alex.js
@@ -16,7 +16,7 @@ function Alex(props) {
         About Me
       </Typography>
       <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-        Hi Everyone, I'm mostly a software developer with experience in System Adminstration and DevOps. I created this website while my partner is in school to have something to do. Here is my resume: <a href="http://kaelnomads-frontend.s3-website-us-east-1.amazonaws.com/public_resources/resume_alex.pdf">link</a>
+        Hi Everyone, I'm mostly a software developer with experience in System Adminstration and DevOps. I created this website while my partner is in school to have something to do. Here is my resume: <a href="https://kaelnomads.com/public_resources/resume_alex.pdf">link</a>
       </Typography>
       <Typography id="transition-modal-description" sx={{ mt: 2 }}>
         Email: a.hsu94@gmail.com
